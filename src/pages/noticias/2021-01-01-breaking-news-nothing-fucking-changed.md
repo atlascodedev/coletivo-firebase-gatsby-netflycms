bae248b59cbfe5abf191a13288256f6084fecce9
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-01-04 20:18:05 -0300

    Netlify CMS field changes

--- src/pages/noticias/2021-01-01-breaking-news-nothing-fucking-changed.md
+++
@@ -1,8 +0,0 @@
----
-templateKey: news-post
-title: Breaking news, nothing fucking changed
-date: 2021-01-01T02:48:40.731Z
-description: Slap that ass
-featuredimage: /img/endocrinologia-foto.png
----
-DING DING
--- src/templates/article-post.jsx
+++ src/templates/article-post.jsx
@@ -1,21 +1,28 @@
-import { graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 
 function ArticlePost(props) {
-  console.log(props)
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(
+        filter: { frontmatter: { contentType: { eq: "teamMembers" } } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              teamMemberName
+              teamMemberPicture
+              teamMemberPosition
+              active
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
 
   return <div>Article Post template</div>
 }
 
 export default ArticlePost
-
-export const articlePostQuery = graphql`
-  query ArticlePost($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-      }
-    }
-  }
-`
