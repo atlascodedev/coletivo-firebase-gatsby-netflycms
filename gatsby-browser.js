5e3e755399d1f213e01c383ff73b73e9568891aa
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-01-26 16:43:06 -0300

    Convert use only Netlify platform

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,5 @@
 import React from "react"
-import firebase from "firebase/app"
-import "firebase/firestore"
-import { FirebaseContext } from "./src/context/firebase"
 import { CssBaseline, MuiThemeProvider } from "@material-ui/core"
-import { config as firebaseConfig } from "./config/firebase.config"
 import { theme } from "./src/theme"
 import "@fontsource/barlow"
 import "@fontsource/barlow/500.css"
@@ -20,25 +16,12 @@ require("swiper/components/effect-fade/effect-fade.min.css")
 require("swiper/components/scrollbar/scrollbar.min.css")
 require("swiper/components/lazy/lazy.min.css")
 
-const app = firebase.initializeApp(firebaseConfig)
-
-const firestore = app.firestore()
-const firestoreNamespace = firebase.firestore
-
-if (process.env.NODE_ENV !== "production") {
-  firestore.useEmulator("localhost", 8080)
-
-  console.log(
-    "Warning, you're running a local instance of Firestore, data will not be persisted to the production database"
-  )
-}
-
 const App = ({ root }) => {
   return (
-    <FirebaseContext.Provider value={{ app, firestore }}>
+    <div>
       <CssBaseline />
       <MuiThemeProvider theme={theme}>{root}</MuiThemeProvider>
-    </FirebaseContext.Provider>
+    </div>
   )
 }
 
